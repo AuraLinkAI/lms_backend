84bc7d788ab727a46fdeec2e41b08764ce80c435
Sarenii <muenisarah159@gmail.com>
2024-12-18 10:20:51 +0300

    auths backend done

---
+++ Lab4GPS/Lab4GPS/urls.py
@@ -0,0 +1,19 @@
+from django.contrib import admin
+from django.urls import path, include
+from django.conf import settings
+from django.conf.urls.static import static
+
+urlpatterns = [
+    # Admin URL
+    path('admin/', admin.site.urls),
+
+    # Auths app URLs
+    path('auth/', include('Auths.urls')),
+
+    # Archive app URLs
+    path('archive/', include('Archive.urls')),  # Added Archive app routes
+]
+
+# Serve media files during development
+if settings.DEBUG:
+    urlpatterns += static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
